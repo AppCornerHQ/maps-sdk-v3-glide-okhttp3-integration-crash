02c5dfb8312e5ed61a2ffb15facb60b52ff19e6d
Tamás Szincsák <dontomika@gmail.com>
2019-05-09 14:07:15 +0200

    Configured Glide v4.9.0 and implemented loading a sample image to test if it works correctly

--- app/src/main/java/eu/appcorner/mapsv3/glideokhttpcrash/MapsActivity.kt
+++ app/src/main/java/eu/appcorner/mapsv3/glideokhttpcrash/MapsActivity.kt
@@ -1,7 +1,9 @@
 package eu.appcorner.mapsv3.glideokhttpcrash
 
 import android.os.Bundle
+import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
+import com.bumptech.glide.Glide
 import com.google.android.libraries.maps.CameraUpdateFactory
 import com.google.android.libraries.maps.GoogleMap
 import com.google.android.libraries.maps.OnMapReadyCallback
@@ -19,6 +21,12 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
 
         val mapFragment = supportFragmentManager.findFragmentById(R.id.map) as SupportMapFragment
         mapFragment.getMapAsync(this)
+
+        // load a sample image
+        val imageView = findViewById<ImageView>(R.id.image)
+        Glide.with(this)
+            .load("https://images-assets.nasa.gov/image/KSC-2014-3456/KSC-2014-3456~orig.jpg")
+            .into(imageView)
     }
 
     override fun onMapReady(googleMap: GoogleMap) {
