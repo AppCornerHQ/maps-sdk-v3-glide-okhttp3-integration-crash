267d193b62886c494e77841e29145f779b47980f
Tamás Szincsák <dontomika@gmail.com>
2019-05-09 14:23:09 +0200

    [CRASH] Removed OkHttp3 integration and added a custom glide module

---
+++ app/src/main/java/eu/appcorner/mapsv3/glideokhttpcrash/CustomGlideModule.java
@@ -0,0 +1,8 @@
+package eu.appcorner.mapsv3.glideokhttpcrash;
+
+import com.bumptech.glide.annotation.GlideModule;
+import com.bumptech.glide.module.AppGlideModule;
+
+@GlideModule
+public class CustomGlideModule extends AppGlideModule {
+}
